0ff02b524d70c048c9894aeb4d35fa653a49ae0b
ciyi <wucaiyimail@foxmail.com>
2023-08-06 16:03:28 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const str = '我爱你祖国'
+console.log(str)
+</script>
+
+<template>
+  <div class="box">
+    <h1>App根组件</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400px;
+  background-color: #000;
+
+  h1 {
+    color: white;
+  }
+}
+</style>
